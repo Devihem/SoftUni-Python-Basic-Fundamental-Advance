ffee17d4039710b32592365aaafa17544e69a93c
Devihem <Devihem7@gmail.com>
2023-01-05 06:25:03 +0200

    .

--- fundamental/02_exercise_data_types_and_variables/9_snowballs.py
+++ fundamental/02_exercise_data_types_and_variables/9_snowballs.py
@@ -1,20 +1,17 @@
 snowballs_count = int(input())
 best_snowball_weight = 0
 best_snowball_time = 0
-best_snowball_value = 0
 best_snowball_quality = 0
-last_snowball_value = 0
+best_snowball_value = 0
 for snowball in range(snowballs_count):
     snowball_weight = int(input())
     snowball_time_needed = int(input())
     snowball_quality = int(input())
     snowball_value = (snowball_weight / snowball_time_needed) ** snowball_quality
-    if snowball_value > last_snowball_value:
-        last_snowball_value = snowball_value
+    if snowball_value > best_snowball_value:
         best_snowball_weight = snowball_weight
         best_snowball_time = snowball_time_needed
-        best_snowball_value = int(snowball_value)
         best_snowball_quality = snowball_quality
-
+        best_snowball_value = int(snowball_value)
 
 print(f"{best_snowball_weight} : {best_snowball_time} = {best_snowball_value} ({best_snowball_quality})")
