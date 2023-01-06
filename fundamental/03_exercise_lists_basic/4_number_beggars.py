984fa40343c28b38ea87a05b490c5d7b8cb0f50e
Devihem <Devihem7@gmail.com>
2023-01-06 22:19:13 +0200

    .

---
+++ fundamental/03_exercise_lists_basic/4_number_beggars.py
@@ -0,0 +1,9 @@
+rewards = input()
+beggars_count = int(input())
+rewards_list = rewards.split(", ")
+beggars_given_rewards = []
+for beggar_order, reward in enumerate(rewards_list):
+    print(beggar_order + 1, reward)
+    beggars_given_rewards.insert()
+
+print(beggars_given_rewards)
--- main.py
+++
@@ -1,4 +0,0 @@
-list_a = ["a", 6, " Lqll", "sdasdas"]
-print(type(list_a[0::]))
-# print(sum(list_a))
-print(list_a)
