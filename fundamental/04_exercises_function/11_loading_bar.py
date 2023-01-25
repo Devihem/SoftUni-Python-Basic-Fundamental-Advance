1957361c7e7f2698c55ff269a03a8d108e3c2a29
Devihem <Devihem7@gmail.com>
2023-01-25 20:54:43 +0200

    .

--- fundamental/04_exercises_function/11_loading_bar.py
+++ fundamental/04_exercises_function/11_loading_bar.py
@@ -1,6 +1,6 @@
-progress_input = int(input())//10
+progress_input = int(input()) // 10
 symbol_1, symbol_2 = "%", "."
 if progress_input == 10:
     print("100% Complete!\n[%%%%%%%%%%]")
 else:
-    print(f"{progress_input*10}% [{progress_input*symbol_1}{(10-progress_input)*symbol_2}]\nStill loading...")
+    print(f"{progress_input * 10}% [{progress_input * symbol_1}{(10-progress_input) * symbol_2}]\nStill loading...")
