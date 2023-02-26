041c4674dd8f2a09b0cd384a8a2c92bd633824fb
Devihem <Devihem7@gmail.com>
2023-02-26 19:27:30 +0200

    .

--- fundamental/08_exercises_text_processing/3_extract_file.py
+++ fundamental/08_exercises_text_processing/3_extract_file.py
@@ -1,4 +1,3 @@
-random_location_string = input()
-folders_in_list = random_location_string.split("\\")
+folders_in_list = input().split("\\")
 file_name, file_extension = folders_in_list[-1].split(".")
 print(f"File name: {file_name}\nFile extension: {file_extension}")
