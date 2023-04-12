869824f1120f18146163506c2182e20d89a49324
Devihem <Devihem7@gmail.com>
2023-04-12 07:49:22 +0300

    .

--- 3_advance/1_exercise_lists_as_stacks_and_queues/2_stacked_queries_softuni.py
+++ 3_advance/1_exercise_lists_as_stacks_and_queues/2_stacked_queries_softuni.py
@@ -1,30 +1,19 @@
 from collections import deque
 
 numbers_stack = deque()
-number_of_commands = int(input())
 
-COMMAND_PUSH = '1'
-COMMAND_DELETE = '2'
-COMMAND_MAXIMUM = '3'
-COMMAND_MINIMUM = '4'
+map_function = {
+    1: lambda x: numbers_stack.append(x[1]),
+    2: lambda x: numbers_stack.pop() if numbers_stack else None,
+    3: lambda x: print(max(numbers_stack)) if numbers_stack else None,
+    4: lambda x: print(min(numbers_stack)) if numbers_stack else None,
+}
 
-for command_number in range(number_of_commands):
 
-    command = input()
-
-    if command.startswith(COMMAND_PUSH):
-        number = command.split()
-        numbers_stack.append(int(number[1]))
-
-    if len(numbers_stack) > 0:
-        if command.startswith(COMMAND_DELETE):
-            numbers_stack.pop()
-
-        elif command.startswith(COMMAND_MAXIMUM):
-            print(max(numbers_stack))
-
-        elif command.startswith(COMMAND_MINIMUM):
-            print(min(numbers_stack))
+for _ in range(int(input())):
+    number_data = [int(x) for x in input().split()]
+    map_function[number_data[0]](number_data)
 
 numbers_stack.reverse()
 print(*numbers_stack, sep=", ")
+print(type(map_function))
