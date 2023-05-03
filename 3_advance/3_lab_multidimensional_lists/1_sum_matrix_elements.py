2975963eeb5b4e017a1ef48f56923173b8a59ee1
Devihem <Devihem7@gmail.com>
2023-05-03 08:21:37 +0300

    .

--- 3_advance/3_lab_multidimensional_lists/1_sum_matrix_elements.py
+++ 3_advance/3_lab_multidimensional_lists/1_sum_matrix_elements.py
@@ -1,9 +1,9 @@
 rows, columns = input().split(", ")
-new_matrix = [input().split(", ") for _ in range(int(rows))]
+new_matrix = [[int(x) for x in input().split(", ")] for _ in range(int(rows))]
 max_sum = 0
 for row in new_matrix:
     for number in row:
-        max_sum += int(number)
+        max_sum += number
 
 print(max_sum)
 print(new_matrix)
