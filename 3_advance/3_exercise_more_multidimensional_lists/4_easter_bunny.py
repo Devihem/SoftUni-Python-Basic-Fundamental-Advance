901cb9d084b47f3993601ec6d5c72ab0af580542
Devihem <Devihem7@gmail.com>
2023-05-15 15:15:03 +0300

    .

--- 3_advance/3_exercise_more_multidimensional_lists/4_easter_bunny.py
+++ 3_advance/3_exercise_more_multidimensional_lists/4_easter_bunny.py
@@ -23,10 +23,13 @@ for row in range(matrix_size):
         if matrix[row][col] == 'B':
             bunny_r, bunny_c = (row, col)
 
+# Checking every direction
 for direction_r, direction_c, name in directions:
     r, c = bunny_r, bunny_c
     current_direction_eggs = 0
     current_direction_eggs_map = []
+
+    # While loop for the current direction
     while 0 <= r < matrix_size and 0 <= c < matrix_size:
         r += direction_r
         c += direction_c
@@ -37,11 +40,13 @@ for direction_r, direction_c, name in directions:
                 current_direction_eggs += int(matrix[r][c])
                 current_direction_eggs_map.append([r, c])
 
+    # Selecting the best path with maximum collected eggs
     if current_direction_eggs > maximum_eggs:
         maximum_eggs = current_direction_eggs
         maximum_eggs_map = current_direction_eggs_map
         winning_direction = name
 
+# Final Print
 if maximum_eggs > 0:
     print(winning_direction)
     print(*maximum_eggs_map, sep='\n')
