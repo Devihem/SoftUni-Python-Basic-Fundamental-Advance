99b4f3f912984b5295c4d17c7d4262e8272f577f
Devihem <Devihem7@gmail.com>
2023-05-23 12:41:40 +0300

    .

--- 3_advance/4_exercise_functions_advanced/9_recursion_palindrome.py
+++ 3_advance/4_exercise_functions_advanced/9_recursion_palindrome.py
@@ -1,16 +1,12 @@
-def palindrome(word: str, first_word):
-    if first_word == 0:
-        first_word = word
+def palindrome(word: str, index):
+    if index == len(word) // 2:
+        return f"{word} is a palindrome"
+    if word[index] != word[-(index + 1)]:
+        return f"{word} is not a palindrome"
 
-    if word[0] == word[-1]:
-        word = word[1:-1]
-        if len(word) > 1:
-            palindrome(word, 0)
-        return f"{first_word} is a palindrome"
-    return f"{first_word} is not a palindrome"
+    return palindrome(word, index + 1)
 
 
 print(palindrome("abcba", 0))
-print(palindrome("peter retep", 0))
+print(palindrome("peter", 0))
 print(palindrome("avvvvvva", 0))
-print(palindrome(" ", 0))
