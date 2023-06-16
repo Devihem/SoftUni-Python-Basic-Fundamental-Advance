e09cebdc76bd8875d4197452360bd32bd0d2c1d0
Devihem <Devihem7@gmail.com>
2023-06-16 23:38:31 +0300

    .

--- 3_advance/exam_14_02_2023/2_blind_man_buff.py
+++ 3_advance/exam_14_02_2023/2_blind_man_buff.py
@@ -1,6 +1,9 @@
-matrix_size = 0
-matrix = [input().split() for x in range(matrix_size)]
+matrix_rows , matrix_cols = map(int,input().split())
 
+matrix = [input().split() for x in range(matrix_rows)]
+
+man_r, man_c = [(r, c) for c in range(matrix_cols) for r in range(matrix_rows) if matrix[r][c] == 'B'][0]
+matrix[man_r][man_c] = '-'
 
 # Type 1 - 4 axis:
 directions = {
@@ -10,10 +13,24 @@ directions = {
     'right': (0, 1)
 }
 
-# Type 2 8-axis:
+other_players = 3
+moves = 0
 
+while other_players > 0:
 
-# Finding UNIT place:
-row_r, row_c = [(row, col)
-                for col in range(matrix_size)
-                for row in range(matrix_size) if map_matrix[row][col] == 'SYMBOL'][0]
+    command = input()
+    if command == 'Finish':
+        break
+    dir_r, dir_c = directions[command]
+
+    new_r = man_r + dir_r
+    new_c = man_c + dir_c
+
+    if 0 <= new_r < matrix_rows and 0 <= new_c < matrix_cols:
+        if matrix[new_r][new_c] != 'O':
+            if matrix[new_r][new_c] == 'P':
+                other_players -= 1
+            moves += 1
+            man_r, man_c = new_r, new_c
+print("Game over!")
+print(f"Touched opponents: {3 - other_players} Moves made: {moves}")
