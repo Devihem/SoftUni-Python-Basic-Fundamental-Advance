5df60635c907b81660b78b3d82978ab3f080ec09
Devihem <Devihem7@gmail.com>
2023-06-16 22:41:56 +0300

    .

--- 3_advance/exam_13_12_2022/2_navy_battle.py
+++ 3_advance/exam_13_12_2022/2_navy_battle.py
@@ -1,6 +1,6 @@
-matrix_size = 0
-matrix = [input().split() for x in range(matrix_size)]
-
+matrix_size = int(input())
+map_matrix = [list(input()) for x in range(matrix_size)]
+sub_r, sub_c = [(r, c) for c in range(matrix_size) for r in range(matrix_size) if map_matrix[r][c] == 'S'][0]
 
 # Type 1 - 4 axis:
 directions = {
@@ -10,10 +10,30 @@ directions = {
     'right': (0, 1)
 }
 
-# Type 2 8-axis:
+# - * C S
+total_ships, total_hp = 3, 3
 
+while total_ships > 0 and total_hp > 0:
+    map_matrix[sub_r][sub_c] = '-'
 
-# Finding UNIT place:
-row_r, row_c = [(row, col)
-                for col in range(matrix_size)
-                for row in range(matrix_size) if map_matrix[row][col] == 'SYMBOL'][0]
+    direction = input()
+
+    new_r, new_c = directions[direction]
+
+    sub_r += new_r
+    sub_c += new_c
+
+    if map_matrix[sub_r][sub_c] == '*':
+        total_hp -= 1
+
+    elif map_matrix[sub_r][sub_c] == 'C':
+        total_ships -= 1
+
+    map_matrix[sub_r][sub_c] = 'S'
+
+if total_ships == 0:
+    print("Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!")
+else:
+    print(f"Mission failed, U-9 disappeared! Last known coordinates [{sub_r}, {sub_c}]!")
+
+[print(''.join(x)) for x in map_matrix]
