5765af1180a4e078f1c49b78665fa4d0a896d676
Devihem <Devihem7@gmail.com>
2023-06-16 12:06:16 +0300

    .

--- 3_advance/exam_11_08_2022/2_crud.py
+++ 3_advance/exam_11_08_2022/2_crud.py
@@ -1,6 +1,7 @@
-matrix_size = 0
+matrix_size = 6
 matrix = [input().split() for x in range(matrix_size)]
 
+loc_r, loc_c = input().strip('(').strip(')').split(', ')
 
 # Type 1 - 4 axis:
 directions = {
@@ -10,8 +11,22 @@ directions = {
     'right': (0, 1)
 }
 
-# Type 2 8-axis:
+while True:
 
+    command_data = input()
+    if command_data == 'Stop':
+        break
+
+    command, direction, *values = input().split(', ')
+
+    if command == "Create":
+        pass
+    elif command == "Update":
+        pass
+    elif command == "Delete":
+        pass
+    elif command == "Read":
+        pass
 
 # Finding UNIT place:
 row_r, row_c = [(row, col)
