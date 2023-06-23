8204328491749b2e1c7ddaa02ceac9800de3d281
Devihem <Devihem7@gmail.com>
2023-06-23 09:30:48 +0300

    .

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-text = "01. Excellent Result02. Greater Number03. Even or Odd04. Password Guess05. Number 100...20006. Speed Info07. Area of Figures"
-text = text.split(".")
-print(text)
+class Peons:
+    def __init__(self, nicknames):
+        self.nicknames = nicknames
 
-fixed_text  = []
 
-for group in text[1:]:
-    print(group)
+georgi = Peons("Rapidfire")
+ivailo = Peons("Devihem")
+
+print(georgi.nicknames)
+georgi.hero = 'Demon'
+print(georgi.__dict__)
+print(ivailo.__dict__)
