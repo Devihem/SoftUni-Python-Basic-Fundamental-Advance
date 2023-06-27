62c6dc4d86934ff3378acb4c4addca74d666fd2c
Devihem <Devihem7@gmail.com>
2023-06-27 21:26:09 +0300

    .

--- oop/encapsulation_lab/email_validator.py
+++ oop/encapsulation_lab/email_validator.py
@@ -5,36 +5,20 @@ class EmailValidator:
         self.domains = domains
 
     def __is_name_valid(self, name):
-        if len(name) >= self.min_length:
-            return True
-        return False
+        return len(name) >= self.min_length
 
     def __is_mail_valid(self, mail):
-        if mail in self.mails:
-            return True
-        return False
+        return mail in self.mails
 
     def __is_domain_valid(self, domain):
-        if domain in self.domains:
-            return True
-        return False
+        return domain in self.domains
 
     def validate(self, email: str):
         name, mail_domain = email.split('@')
         mail, domain = mail_domain.split('.')
+
         check_name = self.__is_name_valid(name)
         check_mail = self.__is_mail_valid(mail)
         check_domain = self.__is_domain_valid(domain)
 
-        if all([check_name, check_mail, check_domain]):
-            return True
-        return False
-
-
-mails = ["gmail", "softuni"]
-domains = ["com", "bg"]
-email_validator = EmailValidator(6, mails, domains)
-print(email_validator.validate("pe77er@gmail.com"))
-print(email_validator.validate("georgios@gmail.net"))
-print(email_validator.validate("stamatito@abv.net"))
-print(email_validator.validate("abv@softuni.bg"))
+        return all([check_name, check_mail, check_domain])
