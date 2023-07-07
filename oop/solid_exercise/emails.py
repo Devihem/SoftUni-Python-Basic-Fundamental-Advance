13712a5a4ef0c6a1c401c5856a9165ae6f0f2eaa
Devihem <devihem7@gmail.com>
2023-07-07 16:12:08 +0300

    .

--- oop/solid_exercise/emails.py
+++ oop/solid_exercise/emails.py
@@ -1,8 +1,63 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 
-class IEmail(object):
-    __metaclass__ = ABCMeta
 
+# -------------------------------SENDER------------------------------------
+class ISender(ABC):
+    @staticmethod
+    @abstractmethod
+    def format(protocol, sender):
+        pass
+
+
+class MySender(ISender):
+    @staticmethod
+    def format(protocol, sender):
+        if protocol == 'IM':
+            return ''.join(["I'm ", sender])
+        return sender
+
+
+# --------------------------------------------------------------------------
+
+
+# -------------------------------RECEIVER------------------------------------
+class IReceiver(ABC):
+    @staticmethod
+    @abstractmethod
+    def format(protocol, receiver):
+        pass
+
+
+class MyReceiver(IReceiver):
+    @staticmethod
+    def format(protocol, receiver):
+        if protocol == 'IM':
+            return ''.join(["I'm ", receiver])
+        return receiver
+
+
+# --------------------------------------------------------------------------
+
+
+# -------------------------------CONTENT------------------------------------
+class IContent(ABC):
+    def __init__(self, text):
+        self.text = text
+
+    @abstractmethod
+    def format(self):
+        pass
+
+
+class MyContent(IContent):
+
+    def format(self):
+        return ''.join(['<myML>', self.text, '</myML>'])
+
+
+# --------------------------------------------------------------------------
+
+class IEmail(ABC):
     @abstractmethod
     def set_sender(self, sender):
         pass
@@ -11,47 +66,45 @@ class IEmail(object):
     def set_receiver(self, receiver):
         pass
 
-    @abstractmethod
-    def set_content(self, content):
-        pass
 
 class Email(IEmail):
 
-    def __init__(self, protocol, content_type):
+    def __init__(self, protocol):
         self.protocol = protocol
-        self.content_type = content_type
         self.__sender = None
         self.__receiver = None
         self.__content = None
 
-    def set_sender(self, sender):
-        if self.protocol == 'IM':
-            self.__sender = ''.join(["I'm ", sender])
-        else:
-            self.__sender = sender
+    def set_sender(self, receiver):
+        self.__sender = MySender.format(self.protocol, receiver)
 
     def set_receiver(self, receiver):
-        if self.protocol == 'IM':
-            self.__receiver = ''.join(["I'm ", receiver])
-        else:
-            self.__receiver = receiver
+        self.__receiver = MyReceiver.format(self.protocol, receiver)
 
     def set_content(self, content):
-        if self.content_type == 'MyML':
-            self.__content = '\n'.join(['<myML>', content, '</myML>'])
-        else:
-            self.__content = content
+        self.__content = content.format()
 
     def __repr__(self):
-
         template = "Sender: {sender}\nReceiver: {receiver}\nContent:\n{content}"
 
-        return template.format(sender = self.__sender, receiver = self.__receiver, content = self.__content)
+        return template.format(sender=self.__sender, receiver=self.__receiver, content=self.__content)
 
 
-email = Email('IM', 'MyML')
+# Official Test
+email = Email('IM')
 email.set_sender('qmal')
 email.set_receiver('james')
-email.set_content('Hello, there!')
+content = MyContent('Hello, there!')
+email.set_content(content)
 print(email)
 
+print()
+print()
+
+# Random Test
+email = Email('GO')
+email.set_sender('qmal')
+email.set_receiver('james')
+content = MyContent('Hello, there!')
+email.set_content(content)
+print(email)
