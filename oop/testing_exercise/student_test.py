3143a6a093f19c64498f72ce413a8fed2c9aa3f4
Devihem <devihem7@gmail.com>
2023-07-14 15:27:03 +0300

    .

---
+++ oop/testing_exercise/student_test.py
@@ -0,0 +1,11 @@
+from student import Student
+from unittest import TestCase
+
+
+class TestStudent(TestCase):
+    def test_entry_data(self):
+        pass
+
+
+if __name__ == "__main__":
+    main()
