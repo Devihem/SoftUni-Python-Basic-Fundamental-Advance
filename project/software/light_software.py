5b664cca4f3294f2975f7503f1c29bb807a9de5d
Devihem <devihem7@gmail.com>
2023-07-17 07:16:32 +0300

    .

--- project/software/light_software.py
+++ project/software/light_software.py
@@ -1,6 +1,9 @@
-from software import Software
+from project.software.software import Software
+from math import floor
 
 
 class LightSoftware(Software):
-    def __init__(self, name: str, software_type, capacity_consumption: int, memory_consumption: int):
-        super().__init__(name, software_type, capacity_consumption, memory_consumption)
+    SOFTWARE_TYPE = 'Light'
+
+    def __init__(self, name: str, capacity_consumption: int, memory_consumption: int):
+        super().__init__(name, self.SOFTWARE_TYPE, floor(capacity_consumption * 1.5), floor(memory_consumption / 2))
