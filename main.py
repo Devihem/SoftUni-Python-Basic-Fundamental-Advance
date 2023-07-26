8931e6391c451bf5fd18f43b0f1f00ac5bb0fdeb
Devihem <devihem7@gmail.com>
2023-07-26 07:31:24 +0300

    .

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-x = 'a sda sd'
-z = x.replace("a","z")
-print(z)
+movies = [[1901 , "A"],[1900 , "Z"],[1900 , "A"],[1900 , "B"],[12200 , "A"]]
+
+result = sorted(movies, key=lambda k: (-k[0], k[1]))
+result = [movie.details() for movie in result]
+result = [] if not result else result
