4703109a6eb3e2d8b6416fa866b148fd97a19f80
Devihem <devihem7@gmail.com>
2023-08-04 17:18:49 +0300

    .

--- main.py
+++ main.py
@@ -4,3 +4,27 @@ if x == "python":
     print("1", x.title())
 
 print("2", x.title())
+x = "python"
+
+if x == "python":
+    print("1", x.title())
+
+print("2", x.title())
+x = "python"
+
+if x == "python":
+    print("1", x.title())
+
+print("2", x.title())
+x = "python"
+
+if x == "python":
+    print("1", x.title())
+
+print("2", x.title())
+x = "python"
+
+if x == "python":
+    print("1", x.title())
+
+print("2", x.title())
